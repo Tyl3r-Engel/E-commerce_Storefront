38b12327672fa395496a8a4d04d610dfbe010262
Minseo Kim <minseo0321@gmail.com>
2022-02-17 12:57:50 -0800

    Related Items Widget Merge

---
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Carousel from './Carousel.jsx';
+
+export default function RelatedItems({ products, productId, setProductId }) {
+  const [relatedProducts, setRelatedProducts] = useState();
+
+  return (
+    <div>
+      <h1 style={{ color: 'red', textAlign: 'center' }}>Over View</h1>
+
+      <h2 style={{ textAlign: 'center' }}>Related Items</h2>
+      <Carousel
+        related
+        products={products}
+        productId={productId}
+        setProductId={setProductId}
+      />
+      <h2 style={{ textAlign: 'center' }}>Your Outfit</h2>
+      <Carousel
+        related={false}
+        products={products}
+        productId={productId}
+        setProductId={setProductId}
+      />
+
+      <h1 style={{ color: 'red', textAlign: 'center' }}>Reviews and Ratings</h1>
+      <h1 style={{ color: 'red', textAlign: 'center' }}>Questsions and Answers</h1>
+    </div>
+  );
+}
