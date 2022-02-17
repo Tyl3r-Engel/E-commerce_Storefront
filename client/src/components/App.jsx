5ae193facc47f3b489029cbf53c5dafc064d48ca
Minseo Kim <minseo0321@gmail.com>
2022-02-17 13:45:39 -0800

    merge resolved 2

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
+/* eslint-disable import/extensions */
+=======
+>>>>>>> newbranch
 import * as React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 import { AppContext } from '../Context';
+<<<<<<< HEAD
+import RelatedItems from './relatedItems/RelatedItems.jsx';
+=======
 import RelatedItems from './RelatedItems.jsx'
+>>>>>>> newbranch
 
 function App() {
   const [currentProduct, setCurrentProduct] = useState({});
@@ -19,12 +27,24 @@ function App() {
 
   return (
     <AppContext.Provider value={currentProduct}>
+<<<<<<< HEAD
+      <h1 style={{color:'red'}}>Product Overview</h1>
+      {/* <h1>{JSON.stringify(currentProduct)}</h1> */}
+      <RelatedItems
+        productId={productId}
+        setProductId={setProductId}
+        currentProduct={currentProduct}
+      />
+      <h1 style={{color:'red'}}>Questions and Answers</h1>
+      <h1 style={{color:'red'}}>Reviews and Ratings</h1>
+=======
       <RelatedItems 
         products={currentProduct.data}
         setProduct={setCurrentProduct}
         productId={productId}
         setProductId={setProductId}  
       />
+>>>>>>> newbranch
     </AppContext.Provider>
   );
 }
