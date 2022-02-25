8050fe46584e6c4660aae00763b4bf337ae7b0be
Robertlh1 <robertlh1@DESKTOP-5JGUGOE.localdomain>
2022-02-25 11:26:37 -0700

    Implemented the hybrid axios method

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const { API_ROUTE, API_KEY } = require('./config');
+const {
+  API_ROUTE, API_KEY1, API_KEY2, API_KEY3, API_KEY4,
+} = require('./config');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+let requestCounter = 1;
+const getApiKey = () => {
+  switch (requestCounter) {
+    case 1:
+      return API_KEY1;
+    case 2:
+      return API_KEY2;
+    case 3:
+      return API_KEY3;
+    case 4:
+      return API_KEY4;
+    default:
+      requestCounter = 2;
+      return API_KEY1;
+  }
+};
 
 app.all('/api/*', async (req, res, next) => {
   try {
@@ -15,12 +34,14 @@ app.all('/api/*', async (req, res, next) => {
       method: `${req.method}`,
       data: req.body,
       headers: {
-        Authorization: API_KEY,
+        Authorization: getApiKey(),
       },
     };
     const { data } = await axios(config);
     res.json(data);
     res.end();
+    console.log('key:', getApiKey(), 'requestCounter:', requestCounter);
+    requestCounter += 1;
     next();
   } catch (error) {
     res.status(error.status || 500).send({
