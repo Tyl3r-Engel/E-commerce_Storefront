a08709936eaa8484d4965e71d5b55b5dcc441779
Tyler Engel <engelcrag@gmail.com>
2022-04-05 11:21:06 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,11 +1,51 @@
-Project Title:
+# E Commerce StoreFront
+### Table of Contents
+You're sections headers will be used to reference location of destination.
 
-Overview:
+- [Description](#description)
+- [How To Use](#how-to-use)
+- [Author Info](#author-info)
 
-Table of Contents:
+---
 
-Description:
+## Description
 
-Installation:
+This application aims to create an E-commerce clothing line store front.
 
-Usage:
+#### Technologies
+
+- React
+- JavaScript
+
+[Back To The Top](#e-commerce-storefront)
+
+---
+
+## How To Use
+
+#### Installation
+```bash
+  $ npm install
+  $ npm run react-build
+  $ npm run uglify
+  $ npm run rollup
+  $ npm run express-dev
+```
+
+
+#### API Reference
+>Inside of the server file a config.js will need to be created.
+```javascript
+  module.exports = {
+  API_ROUTE: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-den/',
+  API_KEY: 'this is where your github token would go',
+  };
+```
+[Back To The Top](#e-commerce-storefront)
+
+---
+
+## Author Info
+- Link to team repo. (https://github.com/HR-DEN17-TEAM-FRODO/FEC_Project_Catwalk)
+
+[Back To The Top](#e-commerce-storefront)
--- server/index.js
+++ server/index.js
@@ -1,31 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const {
-  API_ROUTE, API_KEY1, API_KEY2, API_KEY3, API_KEY4,
-} = require('./config');
+const { API_ROUTE, API_KEY } = require('./config');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-let requestCounter = 1;
-const getApiKey = () => {
-  switch (requestCounter) {
-    case 1:
-      return API_KEY1;
-    case 2:
-      return API_KEY2;
-    case 3:
-      return API_KEY3;
-    case 4:
-      return API_KEY4;
-    default:
-      requestCounter = 2;
-      return API_KEY1;
-  }
-};
 
 app.all('/api/*', async (req, res, next) => {
   try {
@@ -34,13 +15,12 @@ app.all('/api/*', async (req, res, next) => {
       method: `${req.method}`,
       data: req.body,
       headers: {
-        Authorization: getApiKey(),
+        Authorization: API_KEY,
       },
     };
     const { data } = await axios(config);
     res.json(data);
     res.end();
-    requestCounter += 1;
     next();
   } catch (error) {
     res.status(error.status || 500).send({
